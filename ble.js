10469bbc3a35a2f509e3fbbf57185c3ece0d0ecc
anudeep <akarunakaran@mindlogic.com>
2019-05-28 12:27:02 +0530

    ble disconnect

--- ble.js
+++ ble.js
@@ -9,56 +9,77 @@ var myService = 'battery_service';        // fill in a service you're looking fo
 var myCharacteristic = 0xffb2;   // fill in a characteristic from the service here
 
 function connect(){
-  navigator.bluetooth.requestDevice({
-    // filters: [myFilters]       // you can't use filters and acceptAllDevices together
-    optionalServices: [myService],
-    acceptAllDevices: true
+    navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
+  .then(device => device.gatt.connect())
+  .then(server => {
+    // Getting Battery Service...
+    return server.getPrimaryService('battery_service');
   })
-  .then(function(device) {
-    // save the device returned so you can disconnect later:
-    myDevice = device;
-    console.log(device);
-    // connect to the device once you find it:
-    return device.gatt.connect();
+  .then(service => {
+    // Getting Battery Level Characteristic...
+    return service.getCharacteristic('battery_level');
   })
-  .then(function(server) {
-    // get the primary service:
-    return server.getPrimaryService(myService);
+  .then(characteristic => {
+    // Reading Battery Level...
+    return characteristic.readValue();
   })
-  .then(function(service) {
-    // get the  characteristic:
-    return service.getCharacteristics();
+  .then(value => {
+    console.log('Battery percentage is ' + value.getUint8(0));
   })
-  .then(function(characteristics) {
-    // subscribe to the characteristic:
-    for (c in characteristics) {
-      characteristics[c].startNotifications()
-      .then(subscribeToChanges);
-    }
-  })
-  .catch(function(error) {
-    // catch any errors:
-    console.error('Connection failed!', error);
-  });
+  .catch(error => { console.log(error); });
 }
 
-// subscribe to changes from the meter:
-function subscribeToChanges(characteristic) {
-  characteristic.oncharacteristicvaluechanged = handleData;
-}
+// function connect(){
+//   navigator.bluetooth.requestDevice({
+//     // filters: [myFilters]       // you can't use filters and acceptAllDevices together
+//     optionalServices: [myService],
+//     acceptAllDevices: true
+//   })
+//   .then(function(device) {
+//     // save the device returned so you can disconnect later:
+//     myDevice = device;
+//     console.log(device);
+//     // connect to the device once you find it:
+//     return device.gatt.connect();
+//   })
+//   .then(function(server) {
+//     // get the primary service:
+//     return server.getPrimaryService(myService);
+//   })
+//   .then(function(service) {
+//     // get the  characteristic:
+//     return service.getCharacteristics();
+//   })
+//   .then(function(characteristics) {
+//     // subscribe to the characteristic:
+//     for (c in characteristics) {
+//       characteristics[c].startNotifications()
+//       .then(subscribeToChanges);
+//     }
+//   })
+//   .catch(function(error) {
+//     // catch any errors:
+//     console.error('Connection failed!', error);
+//   });
+// }
 
-// handle incoming data:
-function handleData(event) {
-  // get the data buffer from the meter:
-  var buf = new Uint8Array(event.target.value);
-  console.log(buf);
-}
+// // subscribe to changes from the meter:
+// function subscribeToChanges(characteristic) {
+//   characteristic.oncharacteristicvaluechanged = handleData;
+// }
 
-// disconnect function:
-function disconnect() {
-  if (myDevice) {
-    // disconnect:
-    console.log(myDevice)
-    myDevice.gatt.disconnect();
-  }
-}
+// // handle incoming data:
+// function handleData(event) {
+//   // get the data buffer from the meter:
+//   var buf = new Uint8Array(event.target.value);
+//   console.log(buf);
+// }
+
+// // disconnect function:
+// function disconnect() {
+//   if (myDevice) {
+//     // disconnect:
+//     console.log(myDevice)
+//     myDevice.gatt.disconnect();
+//   }
+// }
