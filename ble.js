20429239845524c1812b6d3f3d689228d36415d7
anudeep <akarunakaran@mindlogic.com>
2019-05-29 23:47:04 +0530

    ble

--- ble.js
+++ ble.js
@@ -9,126 +9,45 @@ var myService = 0xffb0;        // fill in a service you're looking for here
 var myCharacteristic = 0xffb2;   // fill in a characteristic from the service here
 
 function connect(){
-  console.log('Requesting any Bluetooth Device...');
-  navigator.bluetooth.requestDevice({
-   // filters: [...] <- Prefer filters to save energy & show relevant devices.
-      acceptAllDevices: true,
-      optionalServices: ['heart_rate']})
-  .then(device => {
-    console.log('Connecting to GATT Server...');
-    return device.gatt.connect();
-  })
-  .then(server => {
-    console.log('Getting heart_rate Service...');
-    return server.getPrimaryService('heart_rate');
-  })
-  .then(service => {
-    console.log('Getting Device Information Characteristics...');
-    return service.getCharacteristics();
-  })
-  .then(characteristics => {
-    let queue = Promise.resolve();
-    let decoder = new TextDecoder('utf-8');
-    characteristics.forEach(characteristic => {
-      switch (characteristic.uuid) {
 
-        case BluetoothUUID.getCharacteristic('heart_rate_measurement'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> heart_rate_measurement: ' + decoder.decode(value));
-          });
-          break;
 
-        // case BluetoothUUID.getCharacteristic('model_number_string'):
-        //   queue = queue.then(_ => characteristic.readValue()).then(value => {
-        //     console.log('> Model Number String: ' + decoder.decode(value));
-        //   });
-        //   break;
+    navigator.bluetooth.requestDevice({ filters: [{ services: ['heart_rate'] }] })
+    .then(device => device.gatt.connect())
+    .then(server => server.getPrimaryService('heart_rate'))
+    .then(service => service.getCharacteristic('heart_rate_measurement'))
+    .then(characteristic => characteristic.startNotifications())
+    .then(characteristic => {
+      characteristic.addEventListener('characteristicvaluechanged',
+                                      handleCharacteristicValueChanged);
+      console.log('Notifications have been started.');
+    })
+    .catch(error => { console.log(error); });
 
-        // case BluetoothUUID.getCharacteristic('hardware_revision_string'):
-        //   queue = queue.then(_ => characteristic.readValue()).then(value => {
-        //     console.log('> Hardware Revision String: ' + decoder.decode(value));
-        //   });
-        //   break;
-
-        // case BluetoothUUID.getCharacteristic('firmware_revision_string'):
-        //   queue = queue.then(_ => characteristic.readValue()).then(value => {
-        //     console.log('> Firmware Revision String: ' + decoder.decode(value));
-        //   });
-        //   break;
-
-        // case BluetoothUUID.getCharacteristic('software_revision_string'):
-        //   queue = queue.then(_ => characteristic.readValue()).then(value => {
-        //     console.log('> Software Revision String: ' + decoder.decode(value));
-        //   });
-        //   break;
-
-        // case BluetoothUUID.getCharacteristic('system_id'):
-        //   queue = queue.then(_ => characteristic.readValue()).then(value => {
-        //     console.log('> System ID: ');
-        //     console.log('  > Manufacturer Identifier: ' +
-        //         padHex(value.getUint8(4)) + padHex(value.getUint8(3)) +
-        //         padHex(value.getUint8(2)) + padHex(value.getUint8(1)) +
-        //         padHex(value.getUint8(0)));
-        //     console.log('  > Organizationally Unique Identifier: ' +
-        //         padHex(value.getUint8(7)) + padHex(value.getUint8(6)) +
-        //         padHex(value.getUint8(5)));
-        //   });
-        //   break;
-
-        // case BluetoothUUID.getCharacteristic('ieee_11073-20601_regulatory_certification_data_list'):
-        //   queue = queue.then(_ => characteristic.readValue()).then(value => {
-        //     console.log('> IEEE 11073-20601 Regulatory Certification Data List: ' +
-        //         decoder.decode(value));
-        //   });
-        //   break;
-
-        // case BluetoothUUID.getCharacteristic('pnp_id'):
-        //   queue = queue.then(_ => characteristic.readValue()).then(value => {
-        //     console.log('> PnP ID:');
-        //     console.log('  > Vendor ID Source: ' +
-        //         (value.getUint8(0) === 1 ? 'Bluetooth' : 'USB'));
-        //     if (value.getUint8(0) === 1) {
-        //       console.log('  > Vendor ID: ' +
-        //           (value.getUint8(1) | value.getUint8(2) << 8));
-        //     } else {
-        //       console.log('  > Vendor ID: ' +
-        //           getUsbVendorName(value.getUint8(1) | value.getUint8(2) << 8));
-        //     }
-        //     console.log('  > Product ID: ' +
-        //         (value.getUint8(3) | value.getUint8(4) << 8));
-        //     console.log('  > Product Version: ' +
-        //         (value.getUint8(5) | value.getUint8(6) << 8));
-        //   });
-        //   break;
-
-        // default: console.log('> Unknown Characteristic: ' + characteristic.uuid);
-      }
-    });
-    return queue;
-  })
-  .catch(error => {
-    console.log('Argh! ' + error);
-  });
+    function handleCharacteristicValueChanged(event) {
+      var value = event.target.value;
+      console.log('Received ' + value);
+      // TODO: Parse Heart Rate Measurement value.
+      // See https://github.com/WebBluetoothCG/demos/blob/gh-pages/heart-rate-sensor/heartRateSensor.js
+    }
 }
 
 // subscribe to changes from the meter:
-function subscribeToChanges(characteristic) {
-  characteristic.oncharacteristicvaluechanged = handleData;
-}
+// function subscribeToChanges(characteristic) {
+//   characteristic.oncharacteristicvaluechanged = handleData;
+// }
 
-/* Utils */
+// // handle incoming data:
+// function handleData(event) {
+//   // get the data buffer from the meter:
+//   var buf = new Uint8Array(event.target.value);
+//   console.log(buf);
+// }
 
-function padHex(value) {
-  return ('00' + value.toString(16).toUpperCase()).slice(-2);
-}
-
-function getUsbVendorName(value) {
-  // Check out page source to see what valueToUsbVendorName object is.
-  return value +
-      (value in valueToUsbVendorName ? ' (' + valueToUsbVendorName[value] + ')' : '');
-}
-
-
-
-
-
+// // disconnect function:
+// function disconnect() {
+//   if (myDevice) {
+//     // disconnect:
+//     console.log(myDevice)
+//     myDevice.gatt.disconnect();
+//   }
+// }
