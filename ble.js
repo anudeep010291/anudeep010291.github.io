041d5aa24de2bbecc0e91e2dc65fd27100ff4a9a
anudeep <akarunakaran@mindlogic.com>
2019-05-30 15:39:09 +0530

    ble-10

--- ble.js
+++ ble.js
@@ -4,18 +4,18 @@ A minimal Web Bluetooth connection example
 created 6 Aug 2018
 by Tom Igoe
 */
+var myDevice;
+var myService = 0xffb0;        // fill in a service you're looking for here
+var myCharacteristic = 0xffb2;   // fill in a characteristic from the service here
 
-
-function connect() {
-  // let serviceUuid = 0x180D;
-  // let serviceUuid = "a3c87500-8ed3-4bdf-8a39-a01bebede295";
-  let serviceUuid = "96d1a1bb-1b81-47b5-88f1-4b5f77b37fee";
-
+function connect(){
+ function onButtonClick() {
+  let serviceUuid = '7f280001-8204-f393-e0a9-e50e24dcca9e';
   // if (serviceUuid.startsWith('0x')) {
   //   serviceUuid = parseInt(serviceUuid);
   // }
 
-  // let characteristicUuid = 0x2A37;
+  let characteristicUuid = '7f280002-8204-f393-e0a9-e50e24dcca9e';
   // if (characteristicUuid.startsWith('0x')) {
   //   characteristicUuid = parseInt(characteristicUuid);
   // }
@@ -26,51 +26,26 @@ function connect() {
     console.log('Connecting to GATT Server...');
     return device.gatt.connect();
   })
-//   .then(server => {
-//     console.log('Getting Service...');
-//     return server.getPrimaryService(serviceUuid);
-//   })
-//   .then(service => {
-//   // Getting Battery Level Characteristic...
-//   return service.getCharacteristic(characteristicUuid);
-// })
-//   .then(characteristic => {
-//   // Reading Battery Level...
-//   return characteristic.readValue();
-// })
-// .then(value => {
-//   console.log('Heart Rate is:: ' + value.getUint8(0));
-// })
-
-
+  .then(server => {
+    console.log('Getting Service...');
+    return server.getPrimaryService(serviceUuid);
+  })
+  .then(service => {
+    console.log('Getting Characteristics...');
+    if (characteristicUuid) {
+      // Get all characteristics that match this UUID.
+      return service.getCharacteristics(characteristicUuid);
+    }
+    // Get all characteristics.
+    return service.getCharacteristics();
+  })
+  .then(characteristics => {
+    console.log('> Characteristics: ' +
+      characteristics.map(c => c.uuid).join('\n' + ' '.repeat(19)));
+  })
   .catch(error => {
     console.log('Argh! ' + error);
   });
 }
 
-
-
-// subscribe to changes from the meter:
-// function subscribeToChanges(characteristic) {
-//   characteristic.oncharacteristicvaluechanged = handleData;
-// }
-
-// // handle incoming data:
-// function handleData(event) {
-//   // get the data buffer from the meter:
-//   var buf = new Uint8Array(event.target.value);
-//   console.console.log(buf);
-// }
-
-// // disconnect function:
-// function disconnect() {
-//   if (myDevice) {
-//     // disconnect:
-//     console.console.log(myDevice)
-//     myDevice.gatt.disconnect();
-//   }
-// }
-
-
-
-// Characteristics: 00002a37-0000-1000-8000-00805f9b34fb
+ }
