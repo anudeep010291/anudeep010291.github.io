0bb903cee491ac627a1ba749c365e55f6414fdca
anudeep <akarunakaran@mindlogic.com>
2019-05-30 14:31:41 +0530

    ble-5

--- ble.js
+++ ble.js
@@ -9,7 +9,7 @@ by Tom Igoe
 function connect() {
   // let serviceUuid = 0x180D;
   // let serviceUuid = "a3c87500-8ed3-4bdf-8a39-a01bebede295";
-  let serviceUuid = 0xff;
+  let serviceUuid = 0xFF;
 
   // if (serviceUuid.startsWith('0x')) {
   //   serviceUuid = parseInt(serviceUuid);
